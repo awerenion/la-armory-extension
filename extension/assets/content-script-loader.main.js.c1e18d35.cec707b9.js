c0496e6420a5ee6859a371367893378109641b82
Roman Zubkov <r.zubkov@rootcode.ru>
2024-01-21 15:15:36 +0300

    new build

--- extension/assets/content-script-loader.main.js.c1e18d35.cec707b9.js
+++ extension/assets/content-script-loader.main.js.c1e18d35.cec707b9.js
@@ -4,7 +4,7 @@
   (async () => {
     await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/main.js.fa6dccb0.js")
+      chrome.runtime.getURL("assets/main.js.c1e18d35.js")
     );
   })().catch(console.error);
 
