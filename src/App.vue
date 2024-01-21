64e0aa016512a69eddbcb1c14abd9f3f57a1a567
Roman Zubkov <r.zubkov@rootcode.ru>
2024-01-21 12:28:00 +0300

    codestyle

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <script setup>
 import SettingsPage from './components/SettingsPage.vue'
-import {ref} from 'vue'
+import { ref } from 'vue'
 
 const tab = ref(null)
 </script>
 
 <template>
-  <div style="min-width:600px">
-    <v-card>
-      <v-tabs
-        v-model="tab"
-        bg-color="primary"
-      >
-        <v-tab value="settings">Settings</v-tab>
-      </v-tabs>
+    <div style="min-width:600px">
+        <v-card>
+            <v-tabs
+                v-model="tab"
+                bg-color="primary"
+            >
+                <v-tab value="settings">Settings</v-tab>
+            </v-tabs>
 
-      <v-card-text>
-        <v-window v-model="tab">
-          <v-window-item value="settings">
-            <SettingsPage/>
-          </v-window-item>
-        </v-window>
-      </v-card-text>
-    </v-card>
-  </div>
+            <v-card-text>
+                <v-window v-model="tab">
+                    <v-window-item value="settings">
+                        <SettingsPage/>
+                    </v-window-item>
+                </v-window>
+            </v-card-text>
+        </v-card>
+    </div>
 </template>
