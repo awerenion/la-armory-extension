21f55bb70804ea10d30569db7eec65b3891523b8
kllx <kllx97@gmail.com>
2024-01-21 17:15:13 +0400

    optimize loadAllCharacters

--- src/content-scripts/modules/gs-personal.js
+++ src/content-scripts/modules/gs-personal.js
@@ -3,30 +3,40 @@ import CharacterList from "../view/CharacterList.vue";
 
 export function loadAllCharacters(charList) {
     const characters = {};
+    var loadedCharactersCount = 0
     charList.forEach((character) => {
         chrome.runtime.sendMessage({type: 'load-character-info', name: character.name}, (response) => {
             characters[character.name] = {
                 gs: response.gs,
                 level: response.level
             };
+            ++loadedCharactersCount
         })
     })
-    setTimeout(() => {
-        const characterList = [];
-        const charactersBlock = document.querySelectorAll('#expand-character-list > ul');
-        charactersBlock.forEach((element, index) => {
-            characterList.push([]);
-            Array.from(element.children).forEach((character) => {
-                const res = {};
-                const charName = character.querySelector("button span").textContent;
-                res.name = charName;
-                res.gs = characters[charName].gs
-                res.level = characters[charName].level;
-                res.class = character.querySelector("button img").getAttribute('alt');
-                res.img = character.querySelector("button img").getAttribute('src');
-                characterList[index].push(res);
-            })
-            createApp(CharacterList, {characterList: characterList[index]}).mount(element);
+    var waitLoadAndDocument = setInterval(function() {
+        if (document && loadedCharactersCount === charList.length) {
+            clearInterval(waitLoadAndDocument)
+            setCharactersGsHtml(characters)
+        }
+    }, 30);
+
+}
+
+const setCharactersGsHtml = (characters) => {
+    const characterList = [];
+    const charactersBlock = document.querySelectorAll('#expand-character-list > ul');
+    charactersBlock.forEach((element, index) => {
+        characterList.push([]);
+        Array.from(element.children).forEach((character) => {
+            const res = {};
+            const charName = character.querySelector("button span").textContent;
+            res.name = charName;
+            res.gs = characters[charName].gs
+            res.level = characters[charName].level;
+            res.class = character.querySelector("button img").getAttribute('alt');
+            res.img = character.querySelector("button img").getAttribute('src');
+            characterList[index].push(res);
         })
-    }, charList.length * 250);
+        createApp(CharacterList, {characterList: characterList[index]}).mount(element);
+    })
 }
