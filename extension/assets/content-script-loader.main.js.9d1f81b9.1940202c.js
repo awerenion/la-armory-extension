8385a52c1c41fc86bb1db4633effa37e4f8847fe
Roman Zubkov <r.zubkov@rootcode.ru>
2024-01-23 16:02:53 +0300

    elixirs

--- extension/assets/content-script-loader.main.js.9d1f81b9.1940202c.js
+++ extension/assets/content-script-loader.main.js.9d1f81b9.1940202c.js
@@ -4,7 +4,7 @@
   (async () => {
     await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/main.js.ff7d9fcd.js")
+      chrome.runtime.getURL("assets/main.js.9d1f81b9.js")
     );
   })().catch(console.error);
 
